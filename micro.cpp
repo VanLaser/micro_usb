10eda8461c3553a12bc8125bb4620d36e7af9ab7
Van Laser <gabi_laser@yahoo.com>
2017-11-20 16:59:48 +0200

    add SPI demo (not working ...)

--- micro.cpp
+++ micro.cpp
@@ -1,30 +1,54 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include <LUFA/Drivers/Peripheral/SPI.h>
+
 // usb debugging
 #include "usb_debug_only.h"
 #include "print.h"
 
-int main() {
-
+inline void init_leds()
+{
 	DDRB |= _BV(PB0); // set PB0 on port B to output
 	DDRD |= _BV(PD5); // set PD5 on port D to output
+}
+
+inline void toggle_leds()
+{
+	PORTD |= _BV(PD5);
+	PORTB &= ~_BV(PB0);
+	_delay_ms(1000); // wait 1s
+
+	PORTD &= ~_BV(PD5);
+	PORTB |= _BV(PB0);
+	_delay_ms(1000);  // wait 1s
+}
+
+
+int main() {
+	const char PROGMEM * print_string = "Sample string\n";
 
 	usb_print_init();
 
-	while(1) {
-		_delay_ms(1000);  // wait 1s
-		PORTD |= _BV(PD5);
-		PORTB &= ~_BV(PB0);
-		_delay_ms(1000); // wait 1s
-		PORTD &= ~_BV(PD5);
-		PORTB |= _BV(PB0);
+	init_leds();
 
+	// SPI loopback test (hookup MOSI to MISO)
+	SPI_Init(SPI_SPEED_FCPU_DIV_64 | SPI_MODE_MASTER);
+
+	while(1) {
 		//puts("test puts\n");
 		//print("test print\n");
 		//println("test println");
 
-		printf("The value of PB0 is: %d\n", PB0);
+		toggle_leds();
+
+		println("Starting SPI transfer:");
+		_delay_ms(100);
+		for (int i = 0; i < 128; ++i)
+			SPI_SendByte(' ');
+		for (const char * pc = print_string; pc != 0; ++pc)
+			pchar(SPI_TransferByte(*pc));
+
 	}
 
 	return 0;
